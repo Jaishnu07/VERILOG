8c132025d17bced2571cbed18c74ac72a6d83284
Jaishnu07 <126758550+Jaishnu07@users.noreply.github.com>
2026-05-23 20:42:43 +0530

    testbench

---
+++ combinational/full_adder/testbench.v
@@ -0,0 +1,33 @@
+module full_adder_tb;
+
+    reg a, b, cin;
+    wire sum, carry;
+
+    // DUT Instantiation
+    full_adder uut (
+        .a(a),
+        .b(b),
+        .cin(cin),
+        .sum(sum),
+        .carry(carry)
+    );
+
+    initial begin
+
+        $monitor("Time=%0t | a=%b b=%b cin=%b | sum=%b carry=%b",
+                  $time, a, b, cin, sum, carry);
+
+        a=0; b=0; cin=0; #10;
+        a=0; b=0; cin=1; #10;
+        a=0; b=1; cin=0; #10;
+        a=0; b=1; cin=1; #10;
+        a=1; b=0; cin=0; #10;
+        a=1; b=0; cin=1; #10;
+        a=1; b=1; cin=0; #10;
+        a=1; b=1; cin=1; #10;
+
+        $finish;
+
+    end
+
+endmodule
