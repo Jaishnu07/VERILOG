9bbe3130ec1eb89b679c28094756dae260f6b591
Jaishnu07 <126758550+Jaishnu07@users.noreply.github.com>
2026-05-23 23:10:27 +0530

    Create code.v

---
+++ combinational/4 to 1 mux/code.v
@@ -0,0 +1,26 @@
+module mux4x1(
+    input a,
+    input b,
+    input c,
+    input d,
+    input [1:0] sel,
+    output reg y
+);
+
+always @(*) begin
+
+    if(sel == 2'b00)
+        y = a;
+
+    else if(sel == 2'b01)
+        y = b;
+
+    else if(sel == 2'b10)
+        y = c;
+
+    else
+        y = d;
+
+end
+
+endmodule
