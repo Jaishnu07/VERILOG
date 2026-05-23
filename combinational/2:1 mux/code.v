d38860b116cd16e09372d6635ce295ac634dec05
Jaishnu07 <126758550+Jaishnu07@users.noreply.github.com>
2026-05-23 23:03:00 +0530

    Create code.v

---
+++ combinational/2:1 mux/code.v
@@ -0,0 +1,18 @@
+module mux2x1(
+    input a,
+    input b,
+    input sel,
+    output reg y
+);
+
+always @(*) begin
+
+    if(sel == 1)
+        y = b;
+
+    else
+        y = a;
+
+end
+
+endmodule
