ea3301831d335b65342a954d05dd54e05f7a6a2a
Jaishnu07 <126758550+Jaishnu07@users.noreply.github.com>
2026-05-24 16:52:32 +0530

    Create testbench.v

---
+++ counters/up counter/testbench.v
@@ -0,0 +1,25 @@
+module up_counter_tb;
+
+    reg clk;
+    wire [3:0] q;
+
+    up_counter uut(
+        .clk(clk),
+        .q(q)
+    );
+
+    initial
+    begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    initial
+    begin
+        $monitor("Time=%0t q=%b", $time, q);
+
+        #100;
+        $finish;
+    end
+
+endmodule
