b304f51ab9e5b378949583e85d2331f7d705f7b0
Jaishnu07 <126758550+Jaishnu07@users.noreply.github.com>
2026-05-24 16:35:56 +0530

    Create code.v

---
+++ sequential/T flipflop/code.v
@@ -0,0 +1,23 @@
+module t_ff(
+    input t,
+    input clk,
+    output reg q
+);
+
+initial
+begin
+    q = 0;
+end
+
+always @(posedge clk)
+begin
+
+    if(t == 0)
+        q <= q;
+
+    else
+        q <= ~q;
+
+end
+
+endmodule
