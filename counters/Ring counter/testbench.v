dfab7a2a4637895d7426533b4ca9e1b11138ea92
Jaishnu07 <126758550+Jaishnu07@users.noreply.github.com>
2026-05-24 17:13:37 +0530

    Update testbench.v

--- counters/Ring counter/testbench.v
+++ counters/Ring counter/testbench.v
@@ -1,1 +1,27 @@
+module ring_counter_tb;
 
+    reg clk;
+    wire [3:0] q;
+
+    ring_counter uut(
+        .clk(clk),
+        .q(q)
+    );
+
+    // Clock Generation
+    initial
+    begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    // Monitor Output
+    initial
+    begin
+        $monitor("Time=%0t q=%b", $time, q);
+
+        #100;
+        $finish;
+    end
+
+endmodule
