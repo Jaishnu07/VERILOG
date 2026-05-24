8f3934ced5302f8c6242482f9c26eeedcec51ba7
Jaishnu07 <126758550+Jaishnu07@users.noreply.github.com>
2026-05-24 17:06:41 +0530

    Update code.v

--- counters/mod N counter/code.v
+++ counters/mod N counter/code.v
@@ -1,1 +1,23 @@
-hello
+// MOD N COUNTER  ( N = 10 )
+module mod10_counter(
+    input clk,
+    output reg [3:0] q
+);
+
+initial
+begin
+    q = 4'b0000;
+end
+
+always @(posedge clk)
+begin
+
+    if(q == 4'b1001)
+        q <= 4'b0000;
+
+    else
+        q <= q + 1;
+
+end
+
+endmodule
