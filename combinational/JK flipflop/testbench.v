3c6a98d1b24a97faf9afd93cd6a1367c54f72bfc
Jaishnu07 <126758550+Jaishnu07@users.noreply.github.com>
2026-05-24 13:01:14 +0530

    Create testbench.v

---
+++ combinational/JK flipflop/testbench.v
@@ -0,0 +1,32 @@
+module jk_ff_tb;
+
+    reg j, k, clk;
+    wire q;
+
+    jk_ff uut(
+        .j(j),
+        .k(k),
+        .clk(clk),
+        .q(q)
+    );
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+
+        $monitor("Time=%0t | j=%b k=%b clk=%b q=%b",
+                  $time, j, k, clk, q);
+
+        j=0; k=0; #10;
+        j=0; k=1; #10;
+        j=1; k=0; #10;
+        j=1; k=1; #10;
+
+        $finish;
+
+    end
+
+endmodule
